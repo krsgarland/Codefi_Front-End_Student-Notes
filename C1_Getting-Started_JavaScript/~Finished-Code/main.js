4db70d672651609104b04d6a9648d5e3b26715ef
WilderDev <76077183+WilderDev@users.noreply.github.com>
2022-02-14 13:05:36 -0600

    ADD: Class 1 Comments

--- C1_Getting-Started_JavaScript/~Finished-Code/main.js
+++ C1_Getting-Started_JavaScript/~Finished-Code/main.js
@@ -1,9 +1,21 @@
-const myHeading = document.querySelector("h1");
-const headingTwo = document.querySelector(".heading-two");
-const bodyElement = document.body;
+// Selects the first <h1> element
+const headingOne = document.querySelector("h1");
 
+// Selects the element with a class name of "heading-two"
+const headingTwo = document.querySelector(".heading-two");
+
+// Selects the body element
+const bodyEl = document.querySelector("body");
+
+// Changes the "headingOne" variable's text content
+headingOne.innerText = "Updated Hello World From JavaScript!";
+
+// Changes the "headingTwo" variable's text color
+headingTwo.style.color = "#134e4a";
+
+// Change the body's background color, and the text color on click
 function updateColors() {
-  bodyElement.style.backgroundColor = "#0c4a6e";
-  myHeading.style.color = "#f0f9ff";
+  bodyEl.style.backgroundColor = "#0c4a6e";
+  headingOne.style.color = "#f0f9ff";
   headingTwo.style.color = "#7dd3fc";
 }
