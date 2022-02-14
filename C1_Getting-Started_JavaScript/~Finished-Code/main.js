d23a53e528b67f9242af4a3e29dd7597bd6caf4f
WilderDev <76077183+WilderDev@users.noreply.github.com>
2022-02-14 13:00:57 -0600

    ADD: Class 1

---
+++ C1_Getting-Started_JavaScript/~Finished-Code/main.js
@@ -0,0 +1,9 @@
+const myHeading = document.querySelector("h1");
+const headingTwo = document.querySelector(".heading-two");
+const bodyElement = document.body;
+
+function updateColors() {
+  bodyElement.style.backgroundColor = "#0c4a6e";
+  myHeading.style.color = "#f0f9ff";
+  headingTwo.style.color = "#7dd3fc";
+}
