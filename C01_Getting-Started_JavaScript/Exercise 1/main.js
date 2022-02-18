fa086b40a73dc1e5477ceacf8388d224aefb0864
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-02-18 14:51:39 -0600

    abc

---
+++ C01_Getting-Started_JavaScript/Exercise 1/main.js
@@ -0,0 +1,20 @@
+const headingOne = document.querySelector ("h1");
+
+const headingTwo = document.querySelector (".heading-two");
+
+const bodyEl = document.querySelector ("body");
+
+headingOne.style.color = "#D64020";
+
+function updateColors() {
+    bodyEl.style.backgroundColor = "#000000";
+    headingOne.style.color = "#209982";
+    headingTwo.style.color = "#9B72B3";
+}
+
+
+
+
+
+
+
